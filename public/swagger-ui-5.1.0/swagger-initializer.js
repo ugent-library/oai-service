99c1ba3cb970b3d26e7cc3d0b14d51f9893dc41a
nsteenla <nicolas.steenlant@ugent.be>
2023-07-14 15:03:07 +0200

    make openapi.yml url relative

--- public/swagger-ui-5.1.0/swagger-initializer.js
+++ public/swagger-ui-5.1.0/swagger-initializer.js
@@ -3,7 +3,7 @@ window.onload = function() {
 
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
-    url: "/api/v1/openapi.yaml",
+    url: "../api/v1/openapi.yaml",
     dom_id: '#swagger-ui',
     deepLinking: true,
     presets: [
